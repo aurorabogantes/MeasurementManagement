7fec2aeaf9e65831cd7cbe55dd08b0b88cd2ab46
Aurora Bogantes <aurorabogantes@gmail.com>
2025-05-05 17:56:31 -0600

    Añadir soporte para Entity Framework y Swagger
    Se ha agregado la referencia a `Microsoft.EntityFrameworkCore.SqlServer` y `Swashbuckle.AspNetCore` en el proyecto. También se ha incluido una referencia al proyecto `MeasurementManagement.DA`.
    
    En `Program.cs`, se han añadido directivas `using`, se ha configurado CORS para permitir todos los orígenes, y se ha configurado el contexto de la base de datos `MeasurementManagementContext` con la cadena de conexión "DefaultConnection". Se han registrado los servicios `IMeasurementManagementBW` y `IMeasuremetManagementDA` como transitorios, y se ha habilitado Swagger en el entorno de desarrollo.

--- MeasurementManagement/Program.cs
+++ MeasurementManagement/Program.cs
@@ -1,4 +1,23 @@
+using MeasurementManagement.BW.CU;
+using MeasurementManagement.BW.Interfaces.BW;
+using MeasurementManagement.BW.Interfaces.DA;
+using MeasurementManagement.DA.Actions;
+using MeasurementManagement.DA.Config;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 // Add services to the container.
 
@@ -7,7 +26,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MeasurementManagementContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<IMeasurementManagementBW, MeasurementManagerBW>();
+builder.Services.AddTransient<IMeasuremetManagementDA, MeasureManagerDA>();
+
 var app = builder.Build();
+
+app.UseCors("AllowAllOrigins");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
