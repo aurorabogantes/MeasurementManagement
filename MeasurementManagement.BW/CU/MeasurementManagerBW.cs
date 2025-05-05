9c73b1285e537342376a8132473140de4960bcc9
Aurora Bogantes <aurorabogantes@gmail.com>
2025-05-05 09:16:59 -0600

    Añadir gestión de medidas y referencia a BC
    Se ha añadido una referencia al proyecto `MeasurementManagement.BC` en `MeasurementManagement.BW.csproj`.
    
    Se ha implementado la clase `MeasurementManagerBW` en `MeasurementManagerBW.cs`, que gestiona la adición, actualización, eliminación y obtención de medidas utilizando `IMeasuremetManagementDA`.
    
    Además, se han creado las interfaces `IMeasurementManagementBW` y `IMeasuremetManagementDA` para definir los métodos necesarios para la gestión de medidas y el acceso a datos, mejorando la separación de preocupaciones y facilitando las pruebas unitarias.

---
+++ MeasurementManagement.BW/CU/MeasurementManagerBW.cs
@@ -0,0 +1,55 @@
+﻿
+using MeasurementManagement.BC.Models;
+using MeasurementManagement.BC.ReglasDeNegocio;
+using MeasurementManagement.BW.Interfaces.BW;
+using MeasurementManagement.BW.Interfaces.DA;
+
+namespace MeasurementManagement.BW.CU
+{
+    public class MeasurementManagerBW : IMeasurementManagementBW
+    {
+        private readonly IMeasuremetManagementDA measuremetManagementDA;
+        public MeasurementManagerBW(IMeasuremetManagementDA measuremetManagementDA)
+        {
+            this.measuremetManagementDA = measuremetManagementDA;
+        }
+        public Task<bool> addMeasure(Measure measure)
+        {
+            return MeasuresRules.theMeasureIsValid(measure) ?
+                measuremetManagementDA.addMeasure(measure) :
+                Task.FromResult(false);
+        }
+
+        public Task<bool> updateMeasure(int id, Measure measure)
+        {
+            if(!MeasuresRules.IdIsValid(id) || !MeasuresRules.theMeasureIsValid(measure))
+            {
+                return Task.FromResult(false);
+            }
+
+            return measuremetManagementDA.updateMeasure(id, measure);
+        }
+
+        public Task<bool> deleteMeasure(int id)
+        {
+            return MeasuresRules.IdIsValid(id) ?
+                measuremetManagementDA.deleteMeasure(id) :
+                Task.FromResult(false);
+        }
+
+        public async Task<Measure> getMeasure(int id)
+        {
+            if(!MeasuresRules.IdIsValid(id))
+            {
+                return null;
+            }
+
+            return await measuremetManagementDA.getMeasure(id);
+        }
+
+        public Task<List<Measure>> getAllMeasures()
+        {
+            return measuremetManagementDA.getAllMeasures();
+        } 
+    }
+}
---
+++ MeasurementManagement.BW/Interfaces/BW/IMeasurementManagementBW.cs
@@ -0,0 +1,14 @@
+﻿
+using MeasurementManagement.BC.Models;
+
+namespace MeasurementManagement.BW.Interfaces.BW
+{
+    public interface IMeasurementManagementBW
+    {
+        Task<bool> addMeasure(Measure measure);
+        Task<bool> updateMeasure(int id, Measure measure);
+        Task<bool> deleteMeasure(int id);
+        Task<Measure> getMeasure(int id);
+        Task<List<Measure>> getAllMeasures();
+    }
+}
