6755bc9d80610da0ecda0fdf1b24ae0dd855646b
Aurora Bogantes <aurorabogantes@gmail.com>
2025-05-05 09:02:32 -0600

    Añadir clase Measure y reglas de validación
    Se ha creado la clase `Measure` en `Measure.cs` con propiedades `Id`, `Name` y `Value`.
    
    En `MeasuresRules.cs`, se ha implementado la clase estática `MeasuresRules` con métodos para validar objetos `Measure` y identificadores.

---
+++ MeasurementManagement.BC/Models/Measure.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MeasurementManagement.BC.Models
+{
+    public class Measure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ MeasurementManagement.BC/ReglasDeNegocio/MeasuresRules.cs
@@ -0,0 +1,21 @@
+﻿
+using MeasurementManagement.BC.Models;
+
+namespace MeasurementManagement.BC.ReglasDeNegocio
+{
+    public static class MeasuresRules
+    {
+        public static bool theMeasureIsValid(Measure measure)
+        {
+            return measure != null &&
+                !string.IsNullOrEmpty(measure.Name) &&
+                measure.Value > 0 &&
+                measure.Id > 0;
+        }
+
+        public static bool IdIsValid(int id)
+        {
+            return id > 0;
+        }
+    }
+}
